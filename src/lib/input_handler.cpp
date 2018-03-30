903902d05f8b77e096d8f1dbb2bcf7ee69c774dd
thereissuchname <thereissuchname@yandex.ru>
2018-03-31 02:30:08 +0300

    refactor input handler

--- src/lib/input_handler.cpp
+++ src/lib/input_handler.cpp
@@ -1,160 +1,156 @@
 #include "input_handler.h"
 
-InputHandler::InputHandler(WindowContext& windowContext)
-	: keyboardModifiers_(0)
-	, mouseModifiers_(0)
-{
-	windowContext.onKeyPress.connect([this](int key, int scancode, int action, int mods) {
-		onKeyPress(key, scancode, action, mods);
-	});
+InputHandler::InputHandler(WindowContext &windowContext)
+        : keyboardModifiers_(0)
+        , mouseModifiers_(0)
+        , window(&windowContext) {
+    windowContext.onKeyPress.connect([this](int key, int scancode, int action, int mods) {
+        onKeyPress(key, scancode, action, mods);
+    });
 
-	windowContext.onMouseMove.connect([this](double x, double y) {
-		onMouseMove(x, y);
-	});
+    windowContext.onMouseMove.connect([this](double x, double y) {
+        onMouseMove(x, y);
+    });
+
+    double xpos, ypos;
+    windowContext.getMousePosition(xpos, ypos);
+
+    setMousePosition(static_cast<ScreenCoord>(xpos), static_cast<ScreenCoord>(ypos));
+    setMouseStartPosition(static_cast<ScreenCoord>(xpos), static_cast<ScreenCoord>(ypos));
 }
 
-void InputHandler::onFrame()
-{
-	setMouseMovedPosition(mouseStart.x, mouseStart.y); // reset moved
+void InputHandler::onFrame() {
+    setMouseMovedPosition(mouseStart.x - mouse.x, mouseStart.y - mouse.y);
+    setMouseStartPosition(mouse.x, mouse.y); // reset start position
 }
 
-bool InputHandler::isPress(KeyCode key)
-{
-	return isKeyPress(key);
+bool InputHandler::isPress(KeyCode key) {
+    return isKeyPress(key);
 }
 
-bool InputHandler::isPress(KeyCode key1, KeyCode key2)
-{
-	return isKeyPress(key1) && isKeyPress(key2);
+bool InputHandler::isPress(KeyCode key1, KeyCode key2) {
+    return isKeyPress(key1) && isKeyPress(key2);
 }
 
-bool InputHandler::isPress(KeyboardModifier modifier, KeyCode key)
-{
-	return isModifierPress(modifier) && isKeyPress(key);
+bool InputHandler::isPress(KeyboardModifier modifier, KeyCode key) {
+    return isModifierPress(modifier) && isKeyPress(key);
 }
 
-bool InputHandler::isPress(MouseModifier modifier)
-{
-	return isModifierPress(modifier); 
+bool InputHandler::isPress(MouseModifier modifier) {
+    return isModifierPress(modifier);
 }
 
-bool InputHandler::isKeyPress(KeyCode key)
-{
-	std::map<KeyCode, bool>::const_iterator it = map_.find(key);
+bool InputHandler::isKeyPress(KeyCode key) {
+    std::map<KeyCode, bool>::const_iterator it = map_.find(key);
 
-	return it != map_.end() && it->second == true;
+    return it != map_.end() && it->second;
 }
 
-bool InputHandler::isModifierPress(KeyboardModifier modifier)
-{
-	return keyboardModifiers_.test(modifier);
+bool InputHandler::isModifierPress(KeyboardModifier modifier) {
+    return keyboardModifiers_.test(modifier);
 }
 
-bool InputHandler::isModifierPress(MouseModifier modifier)
-{
-	return mouseModifiers_.test(modifier);
+bool InputHandler::isModifierPress(MouseModifier modifier) {
+    return mouseModifiers_.test(modifier);
 }
 
-void InputHandler::setKeyDown(KeyCode key)
-{
-	std::map<KeyCode, bool>::iterator it = map_.find(key);
+void InputHandler::setKeyDown(KeyCode key) {
+    auto it = map_.find(key);
 
-	if (it != map_.end()) {
-		it->second = true;
-	} else {
-		map_.insert({ key, true });
-	}
+    if (it != map_.end()) {
+        it->second = true;
+    } else {
+        map_.insert({key, true});
+    }
 }
 
-void InputHandler::setKeyUp(KeyCode key)
-{
-	std::map<KeyCode, bool>::iterator it = map_.find(key);
-	if (it != map_.end()) {
-		it->second = false;
-	}
+void InputHandler::setKeyUp(KeyCode key) {
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+        it->second = false;
+    }
 }
 
-void InputHandler::setModifierDown(KeyboardModifier modifier)
-{
-	keyboardModifiers_.set(modifier, true);
+void InputHandler::setModifierDown(KeyboardModifier modifier) {
+    keyboardModifiers_.set(modifier, true);
 }
 
-void InputHandler::setModifierUp(KeyboardModifier modifier)
-{
-	keyboardModifiers_.set(modifier, false);
+void InputHandler::setModifierUp(KeyboardModifier modifier) {
+    keyboardModifiers_.set(modifier, false);
 }
 
-void InputHandler::setModifierDown(MouseModifier modifier)
-{
-	mouseModifiers_.set(modifier, true);
+void InputHandler::setModifierDown(MouseModifier modifier) {
+    mouseModifiers_.set(modifier, true);
 }
 
-void InputHandler::setModifierUp(MouseModifier modifier) 
-{
-	mouseModifiers_.set(modifier, false);
+void InputHandler::setModifierUp(MouseModifier modifier) {
+    mouseModifiers_.set(modifier, false);
 }
 
-void InputHandler::setMousePosition(int x, int y)
-{
-	mouse.x = x;
-	mouse.y = y;
+void InputHandler::setMousePosition(ScreenCoord x, ScreenCoord y) {
+    mouse.x = x;
+    mouse.y = y;
 }
 
-void InputHandler::setMouseStartPosition(int x, int y)
-{
-	mouseStart.x = x;
-	mouseStart.y = y;
+void InputHandler::setMouseStartPosition(ScreenCoord x, ScreenCoord y) {
+    mouseStart.x = x;
+    mouseStart.y = y;
 }
 
-void InputHandler::setMouseMovedPosition(int x, int y)
-{
-	mouseMoved.x = mouseStart.x - x;
-	mouseMoved.y = mouseStart.y - y;
-
-	mouseStart.x = x;
-	mouseStart.y = y;
+void InputHandler::setMouseMovedPosition(ScreenCoord x, ScreenCoord y) {
+    mouseMoved.x = x;
+    mouseMoved.y = y;
 }
 
-void InputHandler::onKeyPress(int key, int scancode, int action, int mods)
-{
-	switch (action) {
-		case KEY_PRESS:
-			setKeyDown(key);
-			break;
-		case KEY_RELEASE:
-			setKeyUp(key);
-			break;
-	}
+void InputHandler::calcSensetivity(int width, int height, double dpi) {
+    sensetivity.speedFactor = static_cast<float>(dpi) / 1000.0f;
+    sensetivity.sensetivity = static_cast<float>(dpi) / 10000.0f;
 }
 
-void InputHandler::onMouseMove(double x, double y)
-{
-	setMousePosition(x, y);
-	setMouseMovedPosition(x, y);
+void InputHandler::subscribeMouseMove(const std::function<void(double, double)>& f) {
+    window->onMouseMove.connect(f);
 }
 
-void InputHandler::onMouseClick(int button, int state, int x, int y)
-{
-	MouseModifier mouseModifier;
+void InputHandler::subscribeKeyPress(const std::function<void(int, int, int, int)>& f) {
+    window->onKeyPress.connect(f);
+}
 
-	switch (button) {
-	case MOUSE_BUTTON_LEFT:
-		mouseModifier = MouseLeft;
-		break;
-	case MOUSE_BUTTON_MIDDLE:
-		mouseModifier = MouseMiddle;
-		break;
-	case MOUSE_BUTTON_RIGHT:
-		mouseModifier = MouseRight;
-		break;
-	}
+void InputHandler::onKeyPress(int key, int scancode, int action, int mods) {
+    switch (action) {
+        case KEY_PRESS:
+            setKeyDown(key);
+            break;
+        case KEY_RELEASE:
+            setKeyUp(key);
+            break;
+    }
+}
 
-	switch (state) {
-	case KEY_PRESS:
-		setModifierDown(mouseModifier);
-		break;
-	case KEY_RELEASE:
-		setModifierUp(mouseModifier);
-		break;
-	}
+void InputHandler::onMouseMove(double x, double y) {
+    setMousePosition(static_cast<ScreenCoord>(x), static_cast<ScreenCoord>(y));
+}
+
+void InputHandler::onMouseClick(int button, int state, int x, int y) {
+    MouseModifier mouseModifier;
+
+    switch (button) {
+        case MOUSE_BUTTON_LEFT:
+            mouseModifier = MouseLeft;
+            break;
+        case MOUSE_BUTTON_MIDDLE:
+            mouseModifier = MouseMiddle;
+            break;
+        case MOUSE_BUTTON_RIGHT:
+            mouseModifier = MouseRight;
+            break;
+    }
+
+    switch (state) {
+        case KEY_PRESS:
+            setModifierDown(mouseModifier);
+            break;
+        case KEY_RELEASE:
+            setModifierUp(mouseModifier);
+            break;
+    }
 }
--- src/lib/input_handler.h
+++ src/lib/input_handler.h
@@ -11,88 +11,119 @@
 #include "../renderer/window_context.h"
 
 enum KeyboardModifier {
-	KeyboardShift = 1,
-	KeyboardAlt = 2,
-	KeyboardCtrl = 4,
-	KeyboardMeta = 8,
+    KeyboardShift = 1,
+    KeyboardAlt = 2,
+    KeyboardCtrl = 4,
+    KeyboardMeta = 8,
 };
 
 enum MouseModifier {
-	MouseLeft = 1,
-	MouseMiddle = 2,
-	MouseRight = 4,
+    MouseLeft = 1,
+    MouseMiddle = 2,
+    MouseRight = 4,
 };
 
+typedef float ScreenCoord;
+
 struct MousePosition {
-	MousePosition() : x(0), y(0) {}
-	int x;
-	int y;
+    MousePosition() : x(0), y(0) {}
+
+    ScreenCoord x;
+    ScreenCoord y;
 };
 
 typedef short int KeyCode;
 
 struct InputHandler {
-	enum KeyType {
-		KEY_SPACE = GLFW_KEY_SPACE,
-		KEY_ENTER = GLFW_KEY_ENTER,
-		KEY_W = GLFW_KEY_W,
-		KEY_A = GLFW_KEY_A,
-		KEY_S = GLFW_KEY_S,
-		KEY_D = GLFW_KEY_D,
-		KEY_C = GLFW_KEY_C,
-		KEY_ESC = GLFW_KEY_ESCAPE
-	};
+    enum KeyType {
+        KEY_SPACE = GLFW_KEY_SPACE,
+        KEY_ENTER = GLFW_KEY_ENTER,
+        KEY_W = GLFW_KEY_W,
+        KEY_A = GLFW_KEY_A,
+        KEY_S = GLFW_KEY_S,
+        KEY_D = GLFW_KEY_D,
+        KEY_C = GLFW_KEY_C,
+        KEY_ESC = GLFW_KEY_ESCAPE
+    };
 
-	enum KeyAction {
-		KEY_PRESS = GLFW_PRESS,
-		KEY_RELEASE = GLFW_RELEASE,
-		KEY_REPEAT = GLFW_REPEAT
-	};
+    enum KeyAction {
+        KEY_PRESS = GLFW_PRESS,
+        KEY_RELEASE = GLFW_RELEASE,
+        KEY_REPEAT = GLFW_REPEAT
+    };
 
-	enum MouseButton {
-		MOUSE_BUTTON_LEFT = GLFW_MOUSE_BUTTON_LEFT,
-		MOUSE_BUTTON_MIDDLE = GLFW_MOUSE_BUTTON_MIDDLE,
-		MOUSE_BUTTON_RIGHT = GLFW_MOUSE_BUTTON_RIGHT
-	};
+    enum MouseButton {
+        MOUSE_BUTTON_LEFT = GLFW_MOUSE_BUTTON_LEFT,
+        MOUSE_BUTTON_MIDDLE = GLFW_MOUSE_BUTTON_MIDDLE,
+        MOUSE_BUTTON_RIGHT = GLFW_MOUSE_BUTTON_RIGHT
+    };
 
-	InputHandler(WindowContext& windowContext);
+    struct Sensetivity {
+        float sensetivity = 1.0f;
+        float speedFactor = 1.0f;
+        float speed = 1.0f;
+    };
 
-	bool isPress(KeyCode);
-	bool isPress(KeyCode, KeyCode);
-	bool isPress(KeyboardModifier, KeyCode);
-	bool isPress(MouseModifier);
+    explicit InputHandler(WindowContext &windowContext);
 
-	void setKeyDown(KeyCode key);
-	void setKeyUp(KeyCode key);
-	void setModifierDown(KeyboardModifier modifier);
-	void setModifierUp(KeyboardModifier modifier);
+    bool isPress(KeyCode);
 
-	void setMouseDown(MouseModifier modifier);
-	void setMouseUp(MouseModifier modifier);
-	void setMousePosition(int x, int y);
-	void setMouseMovedPosition(int x, int y);
-	void setMouseStartPosition(int x, int y);
-	void setModifierDown(MouseModifier modifier);
-	void setModifierUp(MouseModifier modifier);
+    bool isPress(KeyCode, KeyCode);
 
-	void onKeyPress(int key, int scancode, int action, int mods);
-	void onMouseMove(double x, double y);
-	void onMouseClick(int button, int state, int x, int y);
+    bool isPress(KeyboardModifier, KeyCode);
 
-	void onFrame();
+    bool isPress(MouseModifier);
 
-	MousePosition mouse;
-	MousePosition mouseStart;
-	MousePosition mouseMoved;
+    void setKeyDown(KeyCode key);
+
+    void setKeyUp(KeyCode key);
+
+    void setModifierDown(KeyboardModifier modifier);
+
+    void setModifierUp(KeyboardModifier modifier);
+
+    void setMousePosition(ScreenCoord x, ScreenCoord y);
+
+    void setMouseMovedPosition(ScreenCoord x, ScreenCoord y);
+
+    void setMouseStartPosition(ScreenCoord x, ScreenCoord y);
+
+    void setModifierDown(MouseModifier modifier);
+
+    void setModifierUp(MouseModifier modifier);
+
+    void onKeyPress(int key, int scancode, int action, int mods);
+
+    void onMouseMove(double x, double y);
+
+    void onMouseClick(int button, int state, int x, int y);
+
+    void onFrame();
+
+    void calcSensetivity(int width, int height, double dpi);
+
+    void subscribeMouseMove(const std::function<void(double, double)>& f);
+
+    void subscribeKeyPress(const std::function<void(int, int, int, int)>& f);
+
+    MousePosition mouse;
+    MousePosition mouseStart;
+    MousePosition mouseMoved;
+
+    Sensetivity sensetivity;
 
 private:
-	bool isKeyPress(KeyCode);
-	bool isModifierPress(KeyboardModifier);
-	bool isModifierPress(MouseModifier);
+    bool isKeyPress(KeyCode);
 
-	std::bitset<8> keyboardModifiers_;
-	std::bitset<8> mouseModifiers_;
-	std::map<KeyCode, bool> map_;
+    bool isModifierPress(KeyboardModifier);
+
+    bool isModifierPress(MouseModifier);
+
+    std::bitset<8> keyboardModifiers_;
+    std::bitset<8> mouseModifiers_;
+    std::map<KeyCode, bool> map_;
+
+    WindowContext* window;
 };
 
 #endif
